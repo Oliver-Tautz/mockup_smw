4f604ebc431db35668b76c03f85823e16e5d4a88
Oliver Tautz <oliver.tautz@uni-bielefeld.de>
2022-03-09 18:10:12 +0100

    add semantic install

--- mediawiki_install.sh
+++ mediawiki_install.sh
@@ -19,24 +19,41 @@
 
 set -o nounset                                  # Treat unset variables as an error
 wget https://releases.wikimedia.org/mediawiki/1.37/mediawiki-1.37.1.zip
-sudo pacman -S apache php7-apcu php7-intl  php7-apache mariadb php7-apcu
+sudo pacman -S apache php7-apcu php7-intl  php7-apache mariadb php7-apcu composer
 unzip mediawiki-1.37.1.zip
 sudo mv mediawiki-1.37.1 /srv/http/
-# correct /etc/httpd/conf/extra/httpd-vhosts.conf
 
+# move config files
+sudo cp -r  etc /etc
+
+# correct /etc/httpd/conf/extra/httpd-vhosts.conf
 # uncomment line 67 LoadModule mpm_prefork_module modules/mod_mpm_prefork.so in httpd.conf
 # comment  66 #LoadModule mpm_event_module modules/mod_mpm_event.so in httpd.conf
  # uncomment  919 extension=iconv and  921 extension=intl and  923 extension=mysqli and 927 extension=pdo_mysql in /etc/php7/php.ini
  # uncomment line 1 in /etc/php7/conf.d/apcu.ini
 
 
- sudo mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
- sudo systemctl start mariadb.service
+#config and start mariadb
+sudo mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+sudo systemctl start mariadb.service
+
+
+
+# start apache
+sudo apachectl
 
 # create new user for mariadb!:
     # sudo mysql -u root mysql
     # CREATE USER 'phpadmin'@'localhost' IDENTIFIED BY '132UWEhqwe?!*&as';
     # GRANT ALL PRIVILEGES ON *.* TO 'phpmyadmin'@'localhost' WITH GRANT OPTION;
     # FLUSH PRIVILEGES;
+
+firefox http://localhost/mediawiki-1.37.1/index.php
+
+# install semantic mw extension
+
+cp -r srv /
+cd /srv/http/mediawiki-1.37.1 
+/usr/bin/php7 /usr/bin/composer update --no-dev
 
 # it works :)
